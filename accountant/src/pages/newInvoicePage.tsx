6813584ada10c8b22bb9b298d7de058109b19352
A1iHassan <ali012wkout@gmail.com>
2026-07-17 23:27:21 +0300

    created new invoice form

--- accountant/src/pages/newInvoicePage.tsx
+++ accountant/src/pages/newInvoicePage.tsx
@@ -1,7 +1,16 @@
 const NewInvoicePage = () => {
-	return <>
-		New Invoice 
-	       </>
+	return <div className="flex items-center justify-center h-full">
+		  <form className="flex flex-col gap-4 border rounded-xl p-5">
+		  {/* item name be a search field that shows the available items in the inventory. 
+		      total price should be auto calculated from the single item price and the total count, and it should apply any taxes if available
+		  */}
+		      <input type="text" placeholder="أدخل اسم المنتج"/>
+		      <input type="number" placeholder="أدخل الكمية المطلوبة"/>
+		      <input type="text" placeholder="أدخل سعر القطعة"/>
+		      <input type="text" placeholder="أدخل السعر الإجمالي"/>
+		      <input type="text" placeholder="أدخل اسم العميل"/>
+		  </form> 
+	       </div>
 }
 
-export default NewInvoicePage;
+export default NewInvoicePage
